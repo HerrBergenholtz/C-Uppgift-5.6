c6b9250739785a0b05e0ebee0bb253d4dd13450f
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-24 10:41:50 +0100

    Add project files.

---
+++ Testt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Skriv in tal som du vill multiplicera som är separerade med \"*\"");
+        string[] input = Console.ReadLine().Split("*");
+        int[] tal = new int[input.Length];
+        
+        for (int i = 0; i < input.Length; i++)
+        {
+            tal[i] = int.Parse(input[i]);
+        }
+
+        long produkt = 1;
+
+        for (int i = 0; i < tal.Length; i++)
+        {
+            produkt *= tal[i];
+        }
+        Console.WriteLine(produkt);
+    }
+}
